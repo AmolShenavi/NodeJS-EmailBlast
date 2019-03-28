4b1f26c845b56eaf46aeac43e0ae14bc42a2ae9a
AmolShenavi <46631113+AmolShenavi@users.noreply.github.com>
2019-03-28 15:24:39 +0530

    Update LoggerConfig.js

--- LoggerConfig.js
+++ LoggerConfig.js
@@ -1,44 +1,41 @@
 /*
-        This is config file for Logger
+        This is Logger config file 
 */
 'use strict';
 const { createLogger, format, transports } = require('winston');
 const fs = require('fs');
 const path = require('path');
-const config =require('./config');
+const config = require('./config');
 
 const env = process.env.NODE_ENV || 'development';
 const logDir = config.LogDir;
 
 // Create the log directory if it does not exist
 if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
+    fs.mkdirSync(logDir);
 }
 
 const filename = path.join(config.LogDir, config.LogFile);
 
 const logger = createLogger({
-  // change level if in dev environment versus production
-  level: env === 'development' ? 'debug' : 'info',
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    format.json()
-  ),
-  transports: [
-     new transports.Console({
-      level: 'info',
-      format: format.combine(
-                            format.colorize(),
-                            format.printf(info => `${info.timestamp} ${info.level}: ${info.message} `)
-                            )
-      }),
-    new transports.File({ filename })
-  ]
+    // change level if in dev environment versus production
+    level: env === 'development' ? 'debug' : 'info',
+    format: format.combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        format.json()
+    ),
+    transports: [
+        new transports.Console({
+            level: 'info',
+            format: format.combine(
+                format.colorize(),
+                format.printf(info => `${info.timestamp} ${info.level}: ${info.message} `)
+            )
+        }),
+        new transports.File({ filename })
+    ]
 });
-
-//logger.info('file created '+__dirname+__filename);
-
 //  Export logger so other files can use 
 module.exports = logger;
